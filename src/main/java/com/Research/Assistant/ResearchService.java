53d477f04d749c5b3c34ab750ba1ab686e78c2d2
Abhishekh <rekandlal11@gmail.com>
2026-01-25 22:10:24 +0530

    feat(ResearchService): add service to handle research operations

---
+++ src/main/java/com/Research/Assistant/ResearchService.java
@@ -0,0 +1,11 @@
+package com.Research.Assistant;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResearchService {
+
+    public String processContent(ResearchRequest researchRequest) {
+        return "";
+    }
+}
