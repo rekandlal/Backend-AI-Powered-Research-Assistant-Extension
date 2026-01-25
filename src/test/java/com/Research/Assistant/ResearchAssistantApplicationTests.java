68b66920a90c85a5505fe42b9720c5f714ec969b
Abhishekh <rekandlal11@gmail.com>
2026-01-25 21:40:39 +0530

    Initial commit

---
+++ src/test/java/com/Research/Assistant/ResearchAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Research.Assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
