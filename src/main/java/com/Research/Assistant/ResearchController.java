e2aeca41db25235d05d1170af8e67092bd2f1f90
Abhishekh <rekandlal11@gmail.com>
2026-01-25 21:53:34 +0530

    feat(ResearchController): add API to fetch research data

---
+++ src/main/java/com/Research/Assistant/ResearchController.java
@@ -0,0 +1,11 @@
+package com.Research.Assistant;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/research")
+@CrossOrigin(origins = "*")
+public class ResearchController {
+}
